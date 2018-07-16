0b7c41d04ca634a25651236b93700739a4129957
SLarbiBU <slarbi@bu.edu>
2018-07-16 14:48:18 -0400

    fixes

--- ticTac.py
+++ ticTac.py
@@ -2,54 +2,54 @@ from random import random
 import os
 import time
 
-
-def play():
-    
-    board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']     
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']     
    
-    ########win Flags##########    
-    Win = 1    
-    Draw = -1    
-    Running = 0    
-    Stop = 1    
-    ###########################
+########win Flags##########    
+Win = 1    
+Draw = -1    
+Running = 0    
+Stop = 1    
+###########################
 
-    Game = Running    
-    Mark = 'X'    
-    turn = 1
-    def DrawBoard():    
-        print(" %c | %c | %c " % (board[1],board[2],board[3]))    
-        print("___|___|___")    
-        print(" %c | %c | %c " % (board[4],board[5],board[6]))    
-        print("___|___|___")    
-        print(" %c | %c | %c " % (board[7],board[8],board[9]))  
+Game = Running    
+Mark = 'X'    
+turn = 1
+def DrawBoard():    
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))  
 
 
-    def checkWin():
-        global Game
-        if (board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
-            Game=Win
-        elif (board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
-            Game=Win
-        elif (board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
-            Game=Win
-        elif (board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
-            Game=Win
-        elif (board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
-            Game=Win
-        elif (board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
-            Game=Win
-        elif (board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
-            Game=Win
-        elif (board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
-            Game=Win
-        elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
-            Game=Draw  
-        else:
-            Game = Running
+def checkWin():
+    global Game
+    if (board[1] == board[4] and board[4] == board[7] and board[1] != ' '):
+        Game=Win
+    elif (board[2] == board[5] and board[5] == board[8] and board[2] != ' '):
+        Game=Win
+    elif (board[3] == board[6] and board[6] == board[9] and board[3] != ' '):
+        Game=Win
+    elif (board[1] == board[2] and board[2] == board[3] and board[1] != ' '):
+        Game=Win
+    elif (board[4] == board[5] and board[5] == board[6] and board[4] != ' '):
+        Game=Win
+    elif (board[7] == board[8] and board[8] == board[9] and board[7] != ' '):
+        Game=Win
+    elif (board[1] == board[5] and board[5] == board[9] and board[5] != ' '):
+        Game=Win
+    elif (board[3] == board[5] and board[5] == board[7] and board[5] != ' '):
+        Game=Win
+    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
+        Game=Draw  
+    else:
+        Game = Running
 
     
-
+def play():
+    global Game
+    global turn
+    global Mark
     while(Game == Running):
         Game = Running  
         os.system('cls')
